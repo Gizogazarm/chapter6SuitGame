1c8b1e7faeb52a4d2dcfdd4fce8fe0bf19aa2982
Ishak Cahya <ishakcahya1@gmail.com>
2023-05-19 01:01:01 +0700

    menambahkan shared preference pada fitur profil

--- app/src/main/java/id/rich/challengech5/ProfileActivity.kt
+++ app/src/main/java/id/rich/challengech5/ProfileActivity.kt
@@ -3,6 +3,7 @@ package id.rich.challengech5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import id.rich.challengech5.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -20,12 +21,35 @@ class ProfileActivity : AppCompatActivity() {
     private fun btnClickListener() {
         binding.btnThemeSetting.setOnClickListener {
             startActivity(Intent(this, ThemeActivity::class.java))
-        }
+
+            }
         binding.btnGameHistory.setOnClickListener {
 
-        }
+            }
         binding.btnLogOut.setOnClickListener {
-            startActivity(Intent(this, ThemeActivity::class.java))
+                showLogoutDialog()
+            }
         }
+
+    private fun showLogoutDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Konfirmasi Logout")
+        builder.setMessage("Apakah Anda yakin ingin logout?")
+        builder.setPositiveButton("Ya") { dialog, which ->
+            logout()
+        }
+        builder.setNegativeButton("Tidak", null)
+
+        val dialog = builder.create()
+        dialog.show()
     }
+
+    private fun logout() {
+
+        // menambahkan implementasi yang sesuai untuk menghapus data sesi pengguna,
+        // seperti menghapus token autentikasi atau data sesi lainnya
+
+        startActivity(Intent(this, MenuPageActivity::class.java))
+    }
+
 }
--- app/src/main/java/id/rich/challengech5/ThemeActivity.kt
+++ app/src/main/java/id/rich/challengech5/ThemeActivity.kt
@@ -1,33 +1,52 @@
 package id.rich.challengech5
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
 import id.rich.challengech5.databinding.ActivityThemeBinding
 
 class ThemeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityThemeBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityThemeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        sharedPreferences = getSharedPreferences("ThemePreferences", MODE_PRIVATE)
 
-        binding.rgThemeSetting.setOnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                binding.rbAutoTheme.id -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                }
-                binding.rbLightTheme.id -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                }
-                binding.rbDarkTheme.id -> {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                }
+        val savedTheme = sharedPreferences.getString("theme", "auto")
+        when (savedTheme) {
+            "auto" -> binding.rbAutoTheme.isChecked = true
+            "light" -> binding.rbLightTheme.isChecked = true
+            "dark" -> binding.rbDarkTheme.isChecked = true
+        }
+
+        binding.rgThemeOptions.setOnCheckedChangeListener { _, checkedId ->
+            val theme = when (checkedId) {
+                R.id.rb_autoTheme -> "auto"
+                R.id.rb_lightTheme -> "light"
+                R.id.rb_darkTheme -> "dark"
+                else -> "auto"
             }
+            sharedPreferences.edit {
+                putString("theme", theme)
+                apply()
+            }
+            updateTheme(theme)
+        }
+
+    }
+
+    private fun updateTheme(theme: String) {
+        when (theme) {
+            "auto" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
     }
 }
