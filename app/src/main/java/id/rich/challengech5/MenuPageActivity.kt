8bb17816a1f87ccd1ec3f858ede59988aaac63d7
gizogazarm <theblues11.helmi@gmail.com>
2023-05-18 19:16:11 +0700

    MENAMBAHKAN BUTTON PROFILE AGAR BISA KE HALAMAN PROFILE

--- app/src/main/java/id/rich/challengech5/MenuPageActivity.kt
+++ app/src/main/java/id/rich/challengech5/MenuPageActivity.kt
@@ -27,6 +27,7 @@ class MenuPageActivity : AppCompatActivity() {
         val iv_p2c = findViewById<ImageView>(R.id.iv_p2c)
         val tv_p2p = findViewById<TextView>(R.id.tv_p2p)
         val tv_p2c = findViewById<TextView>(R.id.tv_p2c)
+        val ic_profile = findViewById<ImageView>(R.id.ic_profile)
         val intent = Intent(this, GameActivity::class.java)
 
         tv_p2p.setText("$playername vs Pemain")
@@ -42,6 +43,13 @@ class MenuPageActivity : AppCompatActivity() {
             intent.putExtra("enemy", "CPU")
             intent.putExtra("player_name", playername)
             startActivity(intent)
+        }
+
+        ic_profile.setOnClickListener {
+            ic_profile.setBackgroundResource(R.drawable.bg_button_rounded)
+            Handler(Looper.getMainLooper()).postDelayed({
+                ic_profile.setBackgroundResource(R.drawable.background_btnawal)
+            }, 1000)
         }
     }
 }
