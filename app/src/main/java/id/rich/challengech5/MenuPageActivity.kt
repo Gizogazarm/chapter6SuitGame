6510a9cb2e0b356d8660353b8fc8eaad7c2cc1d4
gizogazarm <theblues11.helmi@gmail.com>
2023-05-18 19:28:46 +0700

    MENGHAPUS IMPORT LOG DI MENUPAGEACTIVITY

--- app/src/main/java/id/rich/challengech5/MenuPageActivity.kt
+++ app/src/main/java/id/rich/challengech5/MenuPageActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
