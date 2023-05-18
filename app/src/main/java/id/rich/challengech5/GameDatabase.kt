7c01fb79ad832169b79364232f14e9d543f15d59
richard <richard.darkazz@gmail.com>
2023-05-18 12:00:53 +0700

    mengupdate 2 db menjadi 1 db

--- app/src/main/java/id/rich/challengech5/GameDatabase.kt
+++ app/src/main/java/id/rich/challengech5/GameDatabase.kt
@@ -5,18 +5,19 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class, GameHistory::class], version = 1)
 
-abstract class UserDatabase: RoomDatabase() {
+abstract class GameDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
+    abstract fun gameHistoryDao(): GameHistoryDao
 
     companion object{
-        private var INSTANCE : UserDatabase? = null
+        private var INSTANCE : GameDatabase? = null
 
-        fun getInstance(context: Context): UserDatabase?{
+        fun getInstance(context: Context): GameDatabase?{
             if(INSTANCE == null){
                 synchronized(this){
-                    INSTANCE = Room.databaseBuilder(context.applicationContext, UserDatabase::class.java,"User.db").build()
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, GameDatabase::class.java,"suit_game.db").build()
                 }
             }
             return INSTANCE
